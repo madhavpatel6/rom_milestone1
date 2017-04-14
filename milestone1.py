348b5a2a51a9508f804dd088ad81abfc8fa0a995
Madhav Patel <patel.v.madhav@gmail.com>
2017-04-14 16:53:31 -0700

    fixed navigation algo

--- milestone1.py
+++ milestone1.py
@@ -5,6 +5,8 @@ from std_msgs.msg import String
 from std_msgs.msg import Int32
 import math
 import pprint
+import warnings
+warnings.filterwarnings("ignore")
 
 pp = pprint.PrettyPrinter(indent=4)
 resolution = rospy.get_param('/grid_step')
@@ -13,13 +15,12 @@ grid_size_y = rospy.get_param('/grid_size_y') + 1
 
 def main():
     rospy.init_node('milestone1')
-    goals = [59, 11, 16, 9]
-    rospy.loginfo('Current configuration is %d by %d with resolution %f' % (grid_size_x, grid_size_y, resolution))
+    goals = [64]
     pub = rospy.Publisher('/goal', Int32, queue_size=10)
     rospy.sleep(2)
-    graph = {}
-    coordinates = {}
-    coordinates[goals[0]] = convert_vertex_to_xy(goals[0])
+    graph, coordinates = create_graph()
+    #coordinates = {}
+    #coordinates[goals[0]] = convert_vertex_to_xy(goals[0])
     rospy.loginfo('Attempting to move to goal position %d' % goals[0])
     while True:
         new_costs, current_vertex = read_edge_costs()
@@ -31,18 +32,32 @@ def main():
                 return
             else:
                 rospy.loginfo('Attempting to move to goal position %d' % goals[0])
-                coordinates[goals[0]] = convert_vertex_to_xy(goals[0])
-        if current_vertex not in graph:
-            coordinates[current_vertex] = convert_vertex_to_xy(current_vertex)
-            for e in new_costs:
-                coordinates[e[1]] = convert_vertex_to_xy(e[1])
-            for c in new_costs:
-                add_to_graph(graph, c[0], c[1], c[2])
-  #      pp.pprint(graph)
-        path, path_cost, itr_count = find_path(current_vertex, goals[0], graph, coordinates, 1, compute_euclidean_distance)
-        for v in path:
-            move_to_vertex(pub, v)
+                #coordinates[goals[0]] = convert_vertex_to_xy(goals[0])
+        for c in new_costs:
+            update_edge_costs(graph, c[0], c[1], c[2])
+        path, path_cost, itr_count = find_path(current_vertex, goals[0], graph, coordinates, 1,
+                                               compute_euclidean_distance)
+        move_to_vertex(pub, path[0])
 
+
+def create_graph():
+    graph = {}
+    coordinates = {}
+    for x in range(1, grid_size_x + 1):
+        for y in range(1, grid_size_y + 1):
+            coordinates[convert_xy_to_vertex(x, y)] = [x, y]
+            if x - 1 >= 1:
+                add_to_graph(graph, convert_xy_to_vertex(x, y), convert_xy_to_vertex(x - 1, y), 1)
+            if y - 1 >= 1:
+                add_to_graph(graph, convert_xy_to_vertex(x, y), convert_xy_to_vertex(x, y - 1), 1)
+            if x + 1 <= grid_size_x:
+                add_to_graph(graph, convert_xy_to_vertex(x, y), convert_xy_to_vertex(x + 1, y), 1)
+            if y + 1 <= grid_size_y:
+                add_to_graph(graph, convert_xy_to_vertex(x, y), convert_xy_to_vertex(x, y + 1), 1)
+    return graph, coordinates
+
+def convert_xy_to_vertex(x, y):
+    return grid_size_y*(x - 1) + y
 
 def convert_vertex_to_xy(v):
     x = math.ceil(float(v)/float(grid_size_y))
@@ -83,15 +98,15 @@ def move_to_vertex(pub, v):
 
 def add_to_graph(graph, key, value, cost):
     if int(key) in graph:
-        graph[int(key)].append((int(value), float(cost)))
+        graph[int(key)].append([int(value), float(cost)])
     else:
-        graph[int(key)] = [(int(value), float(cost))]
+        graph[int(key)] = [[int(value), float(cost)]]
 
-
-def find_path_a_star(start, end, graph, coordinates):
-    print('Performing A Star search with euclidean distance heuristic')
-    return find_path(start, end, graph, coordinates, 1, None)
-
+def update_edge_costs(graph, key, value, cost):
+    for n in graph[key]:
+        if n[0] == value:
+            n[1] = cost
+            break
 
 def compute_euclidean_distance(p1, p2):
     val = math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
@@ -129,41 +144,26 @@ def find_path(start, end, graph, coordinates, weight, heuristic):
                                                                                                   coordinates[end]))
        # print 'Selected node', selected_node
         # Remove it from the open list and store it in the closed list
-
-        if selected_node in graph:
+        open_list.remove(selected_node)
+        closed_list.append(selected_node)
             #print('Min:', selected_node, '->', graph[selected_node])
-            for n in graph[selected_node]:
-                if n[0] not in closed_list:
-                    neighbor = n[0]
-                    cost = n[1]
-                #    print '     neighbor =', neighbor, ' with cost', cost
-                    vnew = cost + cost_to_come[selected_node]
-                #    print '     new cost to come =', vnew
-                    if neighbor not in cost_to_come:
-                        cost_to_come[neighbor] = vnew
-                        back_trace[neighbor] = selected_node
-                    elif vnew < cost_to_come[neighbor]:
-                        #print('Replacing the current entry for', neighbor, 'with old cost', cost_to_come[neighbor - 1],
-                        #      'with new cost', vnew)
-                        cost_to_come[neighbor] = vnew
-                        back_trace[neighbor] = selected_node
-                    if neighbor not in open_list:
-                        open_list.append(neighbor)
-            open_list.remove(selected_node)
-            closed_list.append(selected_node)
-        else:
-     #       print 'Back Trace Table'
-      #      pp.pprint(back_trace)
-      #      rospy.loginfo('Node to visit ' + str(selected_node))
-            path = [selected_node]
-            itr = back_trace[selected_node]
-            while itr != start:
-                path.append(itr)
-                itr = back_trace[itr]
-            path.reverse()
-            rospy.loginfo('Path to take: ' + str(path))
-            show_graph(graph, closed_list, open_list, selected_node, coordinates, start, end)
-            return path, [], itr_count
+        for n in graph[selected_node]:
+            if n[0] not in closed_list:
+                neighbor = n[0]
+                cost = n[1]
+            #    print '     neighbor =', neighbor, ' with cost', cost
+                vnew = cost + cost_to_come[selected_node]
+            #    print '     new cost to come =', vnew
+                if neighbor not in cost_to_come:
+                    cost_to_come[neighbor] = vnew
+                    back_trace[neighbor] = selected_node
+                elif vnew < cost_to_come[neighbor]:
+                    #print('Replacing the current entry for', neighbor, 'with old cost', cost_to_come[neighbor - 1],
+                    #      'with new cost', vnew)
+                    cost_to_come[neighbor] = vnew
+                    back_trace[neighbor] = selected_node
+                if neighbor not in open_list:
+                    open_list.append(neighbor)
         itr_count += 1
 #    print 'Back Trace Tabled'
  #   pp.pprint(back_trace)
@@ -173,14 +173,15 @@ def find_path(start, end, graph, coordinates, weight, heuristic):
         path.append(itr)
         itr = back_trace[itr]
     path.reverse()
-  #  rospy.loginfo('Path to take: ' + str(path))
+    show_graph(graph, closed_list, open_list, path, coordinates, start, end)
+    rospy.loginfo('Path to take: ' + str(path))
     return path, [], itr_count
 
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show_graph(graph, closed, openlist, selected, coordinates, start, end):
+def show_graph(graph, closed, openlist, path, coordinates, start, end):
     plt.ion()
     plt.clf()
 
@@ -211,13 +212,13 @@ def show_graph(graph, closed, openlist, selected, coordinates, start, end):
     nx.draw_networkx_nodes(G, pos, nodelist=not_considered, node_size=100, alpha=1, node_color='gray')
     nx.draw_networkx_nodes(G, pos, nodelist=openlist, node_size=100, alpha=1, node_color='blue')
     nx.draw_networkx_nodes(G, pos, nodelist=closed, node_size=100, alpha=1, node_color='white')
-    nx.draw_networkx_nodes(G, pos, nodelist=[selected], node_size=100, alpha=1, node_color='yellow')
+    nx.draw_networkx_nodes(G, pos, nodelist=path, node_size=100, alpha=1, node_color='yellow')
     nx.draw_networkx_nodes(G, pos, nodelist=[start], node_size=100, alpha=1, node_color='red')
     nx.draw_networkx_nodes(G, pos, nodelist=[end], node_size=100, alpha=1, node_color='green')
     elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == float('inf')]
     esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] != float('inf')]
-    nx.draw_networkx_edges(G, pos, edgelist=elarge, width=2, edge_color='black', style='dashed')
     nx.draw_networkx_edges(G, pos, edgelist=esmall, width=2, edge_color='green')
+    nx.draw_networkx_edges(G, pos, edgelist=elarge, width=2, edge_color='black', style='dashed')
     plt.axis('equal')
     plt.axis('off')
     plt.pause(2)
